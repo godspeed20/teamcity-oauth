fb912c7bb506e16fcdf191d0222886236a56c5e1
godspeed20 <rzkd@hotmail.com>
2018-02-07 23:02:41 +0800

    tests covering group creation

---
+++ src/test/groovy/jetbrains/buildServer/auth/oauth/OAuthUserGroupsTest.groovy
@@ -0,0 +1,80 @@
+package jetbrains.buildServer.auth.oauth
+
+import jetbrains.buildServer.auth.oauth.helpers.StubUserGroup
+import jetbrains.buildServer.groups.UserGroupManager
+import jetbrains.buildServer.serverSide.auth.ServerPrincipal
+import jetbrains.buildServer.web.openapi.PluginDescriptor
+import spock.lang.Specification
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class OAuthUserGroupsTest extends Specification {
+
+    private OAuthClient client = Mock()
+    private HttpServletResponse res = Mock()
+    private OAuthAuthenticationScheme scheme
+
+    private existingGroups = []
+    private createdGroups = []
+
+    def setup() {
+        PluginDescriptor pluginDescriptor = Mock()
+        UserGroupManager userGroupManager = Mock() {
+            findUserGroupByKey(_) >> { String key ->
+                if (!existingGroups.contains(key)) return null
+                return new StubUserGroup(key, key, key)
+            }
+            createUserGroup(_, _, _) >> { String key, String name, String description ->
+                createdGroups.add(key)
+                return new StubUserGroup(key, name, description)
+            }
+        }
+        ServerPrincipalFactory principalFactory = Mock() {
+            getServerPrincipal(_, _) >> { OAuthUser user, boolean allow ->
+                if (allow)
+                    Optional.of(new ServerPrincipal(PluginConstants.OAUTH_AUTH_SCHEME_NAME, user.id))
+                else Optional.empty()
+            }
+        }
+        scheme = new OAuthAuthenticationScheme(pluginDescriptor, principalFactory, userGroupManager, client)
+    }
+
+    def "create non existing roles"() {
+        given:
+        def userName = "testUser"
+        def user = new OAuthUser(userName)
+        HttpServletRequest req = Mock() {
+            getParameter(OAuthAuthenticationScheme.CODE) >> "code"
+            getParameter(OAuthAuthenticationScheme.STATE) >> "state"
+            getRequestedSessionId() >> "state"
+        }
+        client.getAccessToken("code") >> "token"
+        client.getUserData("token") >> user
+        client.getUserRoles(user) >> Optional.of(new OAuthUserRoles([roles: ['role1', 'role2']]))
+        existingGroups = []
+        when:
+        scheme.processAuthenticationRequest(req, res, [:])
+        then:
+        createdGroups == ['role1', 'role2']
+    }
+
+    def "does not create roles when they already exist"() {
+        given:
+        def userName = "testUser"
+        def user = new OAuthUser(userName)
+        HttpServletRequest req = Mock() {
+            getParameter(OAuthAuthenticationScheme.CODE) >> "code"
+            getParameter(OAuthAuthenticationScheme.STATE) >> "state"
+            getRequestedSessionId() >> "state"
+        }
+        client.getAccessToken("code") >> "token"
+        client.getUserData("token") >> user
+        client.getUserRoles(user) >> Optional.of(new OAuthUserRoles([roles: ['role1', 'role2']]))
+        existingGroups = ['role1', 'role2']
+        when:
+        scheme.processAuthenticationRequest(req, res, [:])
+        then:
+        createdGroups == []
+    }
+}
---
+++ src/test/groovy/jetbrains/buildServer/auth/oauth/helpers/StubUserGroup.groovy
@@ -0,0 +1,242 @@
+package jetbrains.buildServer.auth.oauth.helpers
+
+import jetbrains.buildServer.groups.SUserGroup
+import jetbrains.buildServer.groups.UserGroup
+import jetbrains.buildServer.groups.UserGroupException
+import jetbrains.buildServer.notification.DuplicateNotificationRuleException
+import jetbrains.buildServer.notification.NotificationRule
+import jetbrains.buildServer.notification.NotificationRulesHolder
+import jetbrains.buildServer.notification.WatchedBuilds
+import jetbrains.buildServer.serverSide.auth.*
+import jetbrains.buildServer.users.PropertyKey
+import jetbrains.buildServer.users.User
+
+class StubUserGroup implements SUserGroup {
+    private String description
+    private String name
+    private String key
+
+    StubUserGroup(String key, String name, String description) {
+        this.key = key
+        this.name = name
+        this.description = description
+    }
+
+    void setName(String name) throws UserGroupException {
+
+    }
+
+    void setDescription(String description) throws UserGroupException {
+
+    }
+
+    void addSubgroup(UserGroup group) throws UserGroupException {
+
+    }
+
+    void removeSubgroup(UserGroup group) {
+
+    }
+
+    void addUser(User user) {
+
+    }
+
+    void removeUser(User user) throws UserGroupException {
+
+    }
+
+    void setGroupProperties(Map<? extends PropertyKey, String> properties) throws UserGroupException {
+
+    }
+
+    void setGroupProperty(PropertyKey propertyKey, String value) throws UserGroupException {
+
+    }
+
+    void deleteGroupProperty(PropertyKey propertyKey) throws UserGroupException {
+
+    }
+
+    String getKey() {
+        return key
+    }
+
+    String getName() {
+        return name
+    }
+
+    String getDescription() {
+        return description
+    }
+
+    List<UserGroup> getDirectSubgroups() {
+        return null
+    }
+
+    List<UserGroup> getAllSubgroups() {
+        return null
+    }
+
+    List<UserGroup> getParentGroups() {
+        return null
+    }
+
+    List<User> getDirectUsers() {
+        return null
+    }
+
+    List<User> getAllUsers() {
+        return null
+    }
+
+    boolean containsUserDirectly(User user) {
+        return false
+    }
+
+    String describe(boolean verbose) {
+        return null
+    }
+
+    List<NotificationRule> getNotificationRules(String notifierType) {
+        return null
+    }
+
+    void setNotificationRules(String notifierType, List<NotificationRule> rules) {
+
+    }
+
+    void removeRule(long ruleId) {
+
+    }
+
+    void applyOrder(String notifierType, long[] ruleIds) {
+
+    }
+
+    long addNewRule(
+            String notifierType, NotificationRule rule) throws DuplicateNotificationRuleException {
+        return 0
+    }
+
+    Collection<Long> findConflictingRules(String notifierType, WatchedBuilds watch) {
+        return null
+    }
+
+    NotificationRule findRuleById(long ruleId) {
+        return null
+    }
+
+    List<NotificationRulesHolder> getParentRulesHolders() {
+        return null
+    }
+
+    List<NotificationRulesHolder> getAllParentRulesHolders() {
+        return null
+    }
+
+    boolean isPermissionGrantedGlobally(Permission permission) {
+        return false
+    }
+
+    Permissions getGlobalPermissions() {
+        return null
+    }
+
+    Map<String, Permissions> getProjectsPermissions() {
+        return null
+    }
+
+    boolean isPermissionGrantedForProject(String projectId, Permission permission) {
+        return false
+    }
+
+    boolean isPermissionGrantedForAllProjects(Collection<String> projectIds, Permission permission) {
+        return false
+    }
+
+    boolean isPermissionGrantedForAnyProject(Permission permission) {
+        return false
+    }
+
+    boolean isPermissionGrantedForAnyOfProjects(
+            Collection<String> projectIds, Permission permission) {
+        return false
+    }
+
+    Permissions getPermissionsGrantedForProject(String projectId) {
+        return null
+    }
+
+    Permissions getPermissionsGrantedForAllProjects(Collection<String> projectIds) {
+        return null
+    }
+
+    Permissions getPermissionsGrantedForAnyOfProjects(Collection<String> projectIds) {
+        return null
+    }
+
+    User getAssociatedUser() {
+        return null
+    }
+
+    Collection<Role> getRolesWithScope(RoleScope scope) {
+        return null
+    }
+
+    Collection<RoleScope> getScopes() {
+        return null
+    }
+
+    Collection<RoleEntry> getRoles() {
+        return null
+    }
+
+    void addRole(RoleScope scope, Role role) {
+
+    }
+
+    void removeRole(RoleScope scope, Role role) {
+
+    }
+
+    void removeRole(Role role) {
+
+    }
+
+    void removeRoles(RoleScope scope) {
+
+    }
+
+    boolean isSystemAdministratorRoleGranted() {
+        return false
+    }
+
+    boolean isSystemAdministratorRoleGrantedDirectly() {
+        return false
+    }
+
+    boolean isSystemAdministratorRoleInherited() {
+        return false
+    }
+
+    Collection<RolesHolder> getParentHolders() {
+        return null
+    }
+
+    Collection<RolesHolder> getAllParentHolders() {
+        return null
+    }
+
+    String getPropertyValue(PropertyKey propertyKey) {
+        return null
+    }
+
+    boolean getBooleanProperty(PropertyKey propertyKey) {
+        return false
+    }
+
+    Map<PropertyKey, String> getProperties() {
+        return null
+    }
+}
